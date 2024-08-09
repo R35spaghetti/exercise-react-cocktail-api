94a7b568bf710e86f7ea2ac5fb29181e14bafb04
RB <rkrb@tyfonmail.se>
2024-08-09 15:22:23 +0200

    drink interface

---
+++ src/interface.ts
@@ -0,0 +1,13 @@
+export interface IDrink
+{
+    id: number;
+    name: string;
+    tags: string;
+    category: string;
+    type: string;
+    alcoholic: string;
+    glass: string;
+    instructions: string;
+    image: string;
+    measurement: string;
+}
