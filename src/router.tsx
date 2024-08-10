6772b9993f2dfc9fe7b8d5364026ed828e975156
rb <rkrb@tyfonmail.se>
2024-08-10 11:54:15 +0200

    router routes to app

---
+++ src/router.tsx
@@ -0,0 +1,9 @@
+import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
+import {App} from "./components";
+
+export const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App/>}
+        ></Route>
+    )
+)
