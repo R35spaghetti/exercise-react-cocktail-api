fea9a95b4607dea37ca1eb82b9a00a7f9453db99
rb <rkrb@tyfonmail.se>
2024-08-11 12:48:49 +0200

    search hook finding cocktails by name, returns an interface too

---
+++ src/hooks/useSearchCocktails.ts
@@ -0,0 +1,28 @@
+import {useEffect, useState} from "react";
+import {IDrink} from "../interface.ts";
+
+const useSearchCocktails = () => {
+    const [cocktails, setCocktails] = useState<IDrink[] | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [cocktailName, setCocktailName] = useState<string>("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`);
+                const data = await response.json();
+                setCocktails(data.drinks[0]);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            } finally {
+                setLoading(false);
+            }
+
+        };
+        fetchData().catch(error => console.error("Error fetching data", error));
+    }, [cocktailName]);
+
+    return {searchCocktails: cocktails, searchLoading: loading, setSearchCocktailName: setCocktailName};
+
+};
+export default useSearchCocktails;
