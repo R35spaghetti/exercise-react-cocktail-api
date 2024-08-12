b93c0cf2261235987bdf67f2bbdf3ca94d9f593c
rb <rkrb@tyfonmail.se>
2024-08-12 22:51:04 +0200

    reduced IDrink interface. Take the values from the object directly and iterate

--- src/components/CocktailDetailsPage.tsx
+++ src/components/CocktailDetailsPage.tsx
@@ -10,13 +10,32 @@ const CocktailDetailsPage: React.FC = () => {
     } else if (!cocktail) {
         return <div>No cocktails found</div>
     }
+    const measurementsCocktail = Object.entries(cocktail)
+        .filter(([key]) => key.startsWith("strMeasure"))
+        .map(([key, value]) => ({key, value}));
+
+    const ingredientsCocktail = Object.entries(cocktail)
+        .filter(([key]) => key.startsWith("strIngredient"))
+        .map(([key, value]) => ({key, value}));
+
+
     return (
         <article className="cocktail-card">
-            <h2>{cocktail.strDrink}</h2>
-            <p>{cocktail.strInstructions}</p>
+            <h2>category: {cocktail.strCategory}</h2>
+            <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink}/>
+            <p>tags: {cocktail.strTags}</p>
+            <p>glas: {cocktail.strGlass}</p>
+            <h3>Ingredients:</h3>
+            <div className="ingredients-list">
+                {measurementsCocktail.map((entry) => (<p>{entry.value}</p>))}
+                {ingredientsCocktail.map((entry) => (<p>{entry.value}</p>))}
+            </div>
+
+
         </article>
 
 
     )
 }
 export default CocktailDetailsPage;
+
--- src/interface.ts
+++ src/interface.ts
@@ -9,5 +9,4 @@ export interface IDrink
     strGlass: string;
     strInstructions: string;
     strDrinkThumb: string;
-    strMeasurement1: string;
 }
